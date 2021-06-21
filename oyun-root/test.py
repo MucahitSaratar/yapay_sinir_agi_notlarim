caa94c401fc5257f5bd2a5378593f0dc606b3931
MucahitSaratar <trregen222@gmail.com>
2021-06-21 19:54:19 +0300

    biseyler yapildi
    Signed-off-by: MucahitSaratar <trregen222@gmail.com>

--- oyun-root/test.py
+++ oyun-root/test.py
@@ -3,5 +3,11 @@ ag = ysa.ag(4,6,3)
 
 i0 = [1,2,3,4]
 c0 = [0,1,1]
-ag.ogren(i0,c0)
+
+ag.ogren(i0,c0,1)
+print("Hata: ",ag.hata)
+print("cikti: ",ag.degisken)
+ag.ogren(i0,c0,6000)
+print("Hata: ",ag.hata)
+print("cikti: ",ag.degisken)
 
--- oyun-root/ysa.py
+++ oyun-root/ysa.py
@@ -2,7 +2,7 @@ import random as R
 
 class ag():
     def __init__(self,kiv,kps,kpb): # kac input var, katman pec. sifir, katman pec. bir
-        self.oks = 0.0001 # ogrenme kat sayisi
+        self.oks = 0.5 # ogrenme kat sayisi
         self.bias = 3
         self.kpb = kpb
         self.kps = kps
@@ -32,12 +32,19 @@ class ag():
             self.o3.append(self.activation(sum(self.mulliste(self.o2,tek))))
         return self.o3
 
-    def ogren(self,giris,cikis): # [a,b],[a,b]
-        cikti0 = self.tahmin(giris)
-        hatalar = [(cikis[i] - cikti0[i])**2 for i in range(self.kpb)]
-        print(hatalar)
-        for sayi0 in range(self.kpb):
-            hhm = cikis[sayi0] - cikti0[sayi0] # hep hesaplama
-            for ins in range(self.kiv):
-                for katiki in self.katiki:
-                    self.katbir[ins] += 2 * hhm * katiki[ins] * self.giris[ins] * self.oks * 0.006**2
+    def ogren(self,giris,cikis,ts=100): # [a,b],[a,b]
+        for tekrar in range(ts):
+            cikti0 = self.tahmin(giris)
+            self.degisken = cikti0
+            hatalar = [(cikis[i] - cikti0[i])**2 for i in range(self.kpb)]
+            self.hata = hatalar
+            for sayi0 in range(self.kpb):
+                hhm = cikis[sayi0] - cikti0[sayi0] # hep hesaplama
+                for ins in range(self.kiv):
+                    for lan in range(self.kps):
+                        self.katbir[ins] += 2 * hhm * self.katuc[sayi0][lan]  * self.katiki[lan][ins] * self.giris[ins] * self.oks * 0.006**2
+                for iks in range(self.kps):
+                    for i in range(self.kiv):
+                        self.katiki[iks][i] += 2 * hhm * self.katuc[sayi0][iks] * self.o1[i] * self.oks * 0.006**2
+                for bol in range(self.kps):
+                    self.katuc[sayi0][bol] += 2 * hhm * self.o2[bol] * self.oks * 0.006
